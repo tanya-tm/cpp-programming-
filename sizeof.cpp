cc1bc2f12b137346b85136e217a70bb6e9782bba
tanya-tm <63411758+tanya-tm@users.noreply.github.com>
2020-05-05 14:37:38 +0530

    sizeof() in cpp
    this function returns the size of datatypes,variables,constants etc.

---
+++ sizeof.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a;
+	float f;
+	double b;
+	char c;
+	cout<<"size of char: "<<sizeof(c)<<" byte"<<endl;
+	cout<<"size of int: "<<sizeof(a)<<" bytes"<<endl;
+	cout<<"size of float: "<<sizeof(f)<<" bytes"<<endl;
+	cout<<"size of double: "<<sizeof(b)<<" bytes"<<endl;
+}
